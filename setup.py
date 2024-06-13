7a575ad45651d9540a83e699c0380ce09c85db5e
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2024-06-13 18:34:03 +1200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='my_pack',  
+    version='0.1.0',
+    description='',
+    author='author',
+    author_email='email',
+    packages=find_packages(),
+    install_requires=['glob', 'setuptools', 'matplotlib', 'inspect', 're', 'threading', 'subprocess', 'pandas', 'sklearn', 'IPython', 'datetime ', 'shutil', 'seaborn', 'bs4', 'requests', 'json', 'html5lib', 'os', 'numpy', 'io']
+)
