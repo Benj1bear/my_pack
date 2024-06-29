41d102a6ef29dd7982d1ca418cf3e38bece0a1cf
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2024-06-29 16:13:20 +1200

    Update my_pack.py

--- my_pack.py
+++ my_pack.py
@@ -29,6 +29,15 @@ import sys
 
 
 ### how to extend a class by another class ###
+# define your new methods; we should just be able to inherit and use simple methods
+# when we inherit from pd.DataFrame that becomes the 'self' so self is the dataframe
+# we'll be manipulating
+class str_df(pd.DataFrame):
+    def __getitem__(self,index):
+        return self.map(lambda x:x[index])
+# example:
+# df=pd.DataFrame([["hi there","hi there"],["hi yes","hi yes"]])
+# df.str[0:6]
 @property # basically this just means we can do ._str rather than ._str() e.g. it's a property, 
 # otherwise we have to constantly wrap in our custom object to instantize it
 def class_extend(data):
@@ -43,15 +52,7 @@ def class_extend(data):
 # add new property
 CLASS,pd.DataFrame.str=str_df,class_extend
 
-# define your new methods; we should just be able to inherit and use simple methods
-# when we inherit from pd.DataFrame that becomes the 'self' so self is the dataframe
-# we'll be manipulating
-class str_df(pd.DataFrame):
-    def __getitem__(self,index):
-        return self.map(lambda x:x[index])
-# example:
-# df=pd.DataFrame([["hi there","hi there"],["hi yes","hi yes"]])
-# df.str[0:6]
+
 #########################################
 def req_file(directory=""):
     """writes a requirements .txt file for .py and .ipynb files with modules version 
