129a087f69efe6b862cb0cd99d42407f73fc1b7a
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2024-07-12 22:55:39 +1200

    Update my_pack.py

--- my_pack.py
+++ my_pack.py
@@ -48,7 +48,7 @@ def refresh()->None:
     """Refreshes jupyter notebook; it will save the current page as well"""
     display(Javascript("Jupyter.notebook.save_checkpoint();window.onbeforeunload=null;location.reload();"))
 
-def dynamic_js_wrapper(func: Callable[bool])->None:
+def dynamic_js_wrapper(func: Callable[bool,...])->None:
     """wrapper function for dynamically created javascript functions to save code"""
     def wrapper(reload: bool=False):
         """wrapper ensures no appending of duplicate scripts and reloading if necessary"""
