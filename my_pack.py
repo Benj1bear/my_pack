baf1b238251ab6c6be0abae47602063b459ba32b
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2024-07-26 17:26:57 +1200

    Update my_pack.py

--- my_pack.py
+++ my_pack.py
@@ -44,6 +44,9 @@ class dct_ext:
     # should print
     # {'a': 4, 'b': 5}
     # you can also slice using ["a":"b"] etc.
+    # it also allows multiple assignments i.e.
+    dct_ext(rcParams)['figure.figsize','font.size','figure.dpi','lines.linewidth','axes.facecolor','patch.edgecolor','font.family']=\
+    (0,2.7),3.1,58,42,'colour1','colour2','style1'
     """
     def __init__(self,dct: dict) -> None:
         self.dct=dct
