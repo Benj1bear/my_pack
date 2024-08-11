495c0a4f8cb7cf5f3d769e718a879e47dabe8f77
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2024-08-12 10:55:59 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     author='author',
     author_email='email',
     packages=find_packages(),
-    install_requires=['numpy','bs4','seaborn','scikit-learn','requests','setuptools','matplotlib','IPython','pandas']
+    install_requires=['numpy','bs4','seaborn','scikit-learn','requests','setuptools','matplotlib','IPython','pandas','pyautogui']
 )
