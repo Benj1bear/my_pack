376e4a05c0cf65b64041a7eacb8146642137ab0a
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2024-10-21 10:22:17 +1300

    Update my_pack.py

--- my_pack.py
+++ my_pack.py
@@ -61,7 +61,7 @@ def load_notebook_url() -> None:
     display(Javascript("""IPython.notebook.kernel.execute("NOTEBOOK_URL='"+window.location.href+"'")"""))
 
 load_notebook_url()
-
+## doesn't work in library because of the executing/async problem
 def IPython_getcwd() -> str:
     """Gets the full file path if using jupyter notebook"""
     return os.getcwd()+"\\"+NOTEBOOK_URL.split("/")[-1]
