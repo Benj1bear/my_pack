0d716ee49b7e778279d2f09a84ce3850d3ca4839
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2024-10-20 16:08:57 +1300

    Update my_pack.py

--- my_pack.py
+++ my_pack.py
@@ -51,6 +51,17 @@ import dill
 import ctypes
 from copy import deepcopy
 
+def unwrap(FUNC: Callable) -> tuple[Callable,...]:
+    """Extracts the function and all its wrapper functions in execution order"""
+    functions=(decorated,)
+    while True:
+        try:
+            FUNC=FUNC.__closure__[0].cell_contents
+            functions+=(FUNC,)
+        except:
+            break
+    return functions
+
 class readonly:
     """allows readonly attributes"""
     def __init__(self, fget) -> None: self.fget,self.__doc__=fget,fget.__doc__
