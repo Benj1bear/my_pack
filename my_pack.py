75ad9bf716255fc38307105bfedde4a4fac39527
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2024-10-25 23:10:33 +1300

    Update my_pack.py

--- my_pack.py
+++ my_pack.py
@@ -100,9 +100,12 @@ def wrangle_source(True_name: str,True_module: str="__main__") -> str:
     """
     ## need to do something about the history function in case of exceptions because they get recorded otherwise it should work fine
     source=history(True) if True_module=="__main__" else open(__import__(True_module).__file__).read()
-    source_code=extract_callables(True_name,source)[-1] ## get the last defined version
-    lines=slice(*source_code[0][True_name])
-    return "\n".join(source.split("\n")[lines])
+    source_code=extract_callables(True_name,source)
+    if source_code:
+        source_code=source_code[-1] ## get the last defined version
+        lines=slice(*source_code[0][True_name])
+        return "\n".join(source.split("\n")[lines])
+    raise Exception(f"Source code for callable '{True_name}' not found in module '{True_module}'")
 
 def history(join: bool=False) -> list[str]:
     """
