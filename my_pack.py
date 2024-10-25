f4564fcbc5fde24f5e50a4545645919606d90afc
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2024-10-26 00:25:56 +1300

    Update my_pack.py

--- my_pack.py
+++ my_pack.py
@@ -86,7 +86,7 @@ def ast_annotate(node: ast) -> str:
 
 def extract_callables(True_name: str,True_module: str) -> str:
     """Gets all callables from a string"""
-    source=history(True) if True_module=="__main__" else open(__import__(True_module).__file__).read()
+    source=history(True) if True_module=="__main__" else open(__import__(True_module).__file__,encoding="utf-8").read()
     ## TODO: will need to also add in the parameters e.g. to allow knowledge on how many there are etc.
     return source,[({True_name:(obj.lineno-1,obj.end_lineno)}|{decorator.id:(decorator.lineno-1,decorator.end_lineno) for decorator in obj.decorator_list[::-1]},ast_signature(obj)) 
             for obj in ast.parse(source).body if isinstance(obj,ast.FunctionDef|ast.ClassDef) and obj.name==True_name]
