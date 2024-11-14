021966f3ab40a5b7ab2408d68752f655b10d9e8d
Benj1bear <131564813+Benj1bear@users.noreply.github.com>
2024-11-15 11:05:29 +1300

    Update my_pack.py

--- my_pack.py
+++ my_pack.py
@@ -128,14 +128,20 @@ class pickle_stack:
     from copyreg import _extension_registry, _inverted_registry, _extension_cache ## only needed for this class
     
     #### use single underscore for names for access and separation (i.e. on dir usage) ####
-    def _next_buffer(self,arg) -> Any: ## set to a property type if no arg needed
+
+    def _next_buffer(self,arg) -> Any:
         """gets a buffer object for out of band buffers"""
         return next(self.buffers)
 
     def _readonly_buffer(self,arg) -> Any:
-        """sets the buffer to readonly (the buffer will be the top item on the stack)"""
-        ## should work but not sure what other cases are yet
-        return bytes(self.stack.pop())
+        """
+        sets the buffer to readonly (the buffer will be the top item on the stack)
+        code reference: Python Software Foundation. (2024). Python. 3.13. https://github.com/python/cpython/blob/cae9d9d20f61cdbde0765efa340b6b596c31b67f/Lib/pickle.py#L1521C1-L1525C48
+        # changes made: returned as a value instead of appending
+        """
+        buf = self.stack.pop()
+        with memoryview(buf) as m:
+            if not m.readonly: return m.toreadonly()
     
     # for determining opcode type
     _opcode_map={opcode.name:index for index,opcode in enumerate(pickletools.opcodes)}
